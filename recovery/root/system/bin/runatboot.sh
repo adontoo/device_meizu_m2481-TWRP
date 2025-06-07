536ae8a5625f2c77654ddd1df302cd9899261c9d
adontoo <1056651908@qq.com>
2025-06-07 08:52:38 +0800

    修复电池电量显示

--- recovery/root/system/bin/runatboot.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-setprop sys.usb.config mtp,adb
-setenforce 0
-mount /dev/block/bootdevice/by-name/mzlogfs /mzlogfs
-mkdir -p /tmp/mz
-mount /dev/block/bootdevice/by-name/mzlogfs /tmp/mz
-logcat -f /mzlogfs/logcat.txt
-dmesg -w > /mzlogfs/dmesg.txt
-exit 0
